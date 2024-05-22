965dfc646136977b20cc2da35cdbb94abf8015d4
Jeeva463 <jeeva463852@gmail.com>
2024-05-22 15:04:42 +0530

    Completed_Login_Page

--- src/main/java/com/example/resale/service/UserService.java
+++ src/main/java/com/example/resale/service/UserService.java
@@ -6,12 +6,15 @@ import com.example.resale.entity.User;
 import com.example.resale.enums.Status;
 import com.example.resale.enums.Usertype;
 import com.example.resale.repository.UserRepository;
+import com.example.resale.util.PasswordUtil;
+
 import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UserService {
 	private final  UserRepository userRepository;
-	private final BCryptPasswordEncoder passwordEncoder;
+	//miss---S
+	//private final BCryptPasswordEncoder passwordEncoder;
 	//public User userRegister(User user) {
 //		return userRepository.save(user);
 		
@@ -28,8 +31,10 @@ public class UserService {
 				.gender(user.getGender())
 				.status(Status.ACTIVE)
 				.role(Usertype.USER)
-				.password(passwordEncoder.encode(user.getPassword()))
-				.conformPassword(passwordEncoder.encode(user.getConformPassword()))
+				.password(PasswordUtil.getEncryptedPassword(user.getPassword()))
+				.conformPassword(PasswordUtil.getEncryptedPassword(user.getConformPassword()))
+//				.password(user.getPassword())
+//				.conformPassword(user.getConformPassword())
 				.mobileNo(user.getMobileNo()).build();
 				//return user;
 		
